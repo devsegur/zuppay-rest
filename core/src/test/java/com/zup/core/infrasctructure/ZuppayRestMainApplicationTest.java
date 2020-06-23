d92c921702849c6524676c991a4dcc68f2af5cb3
evertonfs <evertonsegur5@gmail.com>
2020-06-23 20:16:44 -0300

    feat(spring init) configured resources with database connection

---
+++ core/src/test/java/com/zup/core/infrasctructure/ZuppayRestMainApplicationTest.java
@@ -0,0 +1,14 @@
+package com.zup.core.infrasctructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuppayRestMainApplicationTest {
+
+  @Test
+  void grantApplicationStartup() {
+
+  }
+
+}
