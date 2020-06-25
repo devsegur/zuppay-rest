78cd5ec82c312857f952f42842f5cf4c21be6b12
evertonfs <evertonsegur5@gmail.com>
2020-06-25 01:20:15 -0300

    feat(service credit card) configured according crud model

---
+++ core/src/main/java/com/zup/domain/service/CreditCardService.java
@@ -0,0 +1,43 @@
+package com.zup.domain.service;
+
+import com.zup.domain.dto.CreditCardDTO;
+import com.zup.domain.mapper.CreditCardMapper;
+import com.zup.infrasctructure.repository.CreditCardRepository;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CreditCardService implements CrudService<CreditCardDTO> {
+
+  private final CreditCardRepository repository;
+  private final CreditCardMapper mapper;
+
+  @Override
+  public List<CreditCardDTO> listAll() {
+    return repository.findAll().parallelStream().map(mapper::map).collect(Collectors.toList());
+  }
+
+  @Override
+  public CreditCardDTO findOneById(UUID uuid) {
+    return repository.findById(uuid).map(mapper::map).orElse(null);
+  }
+
+  @Override
+  public CreditCardDTO save(CreditCardDTO dto) {
+    return null;
+  }
+
+  @Override
+  public CreditCardDTO update(CreditCardDTO dto) {
+    return null;
+  }
+
+  @Override
+  public CreditCardDTO delete(CreditCardDTO dto) {
+    return null;
+  }
+}
