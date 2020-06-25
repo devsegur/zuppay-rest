223bcc1cf07d61f819b8de0b871608e5994d7135
evertonfs <evertonsegur5@gmail.com>
2020-06-24 21:31:47 -0300

    feat(service interface) configured according crud model

--- core/src/main/java/com/zup/infrasctructure/repository/CreditCardRepository.java
+++ core/src/main/java/com/zup/infrasctructure/repository/CreditCardRepository.java
@@ -1,9 +1,9 @@
 package com.zup.infrasctructure.repository;
 
-import com.zup.domain.entity.BaseEntity;
+import com.zup.domain.entity.CreditCard;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CreditCardRepository extends JpaRepository<BaseEntity, UUID> {}
+public interface CreditCardRepository extends JpaRepository<CreditCard, UUID> {}
