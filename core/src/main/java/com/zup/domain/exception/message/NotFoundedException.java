fbce837e51def9c935c26843ddd7618fc8a262bb
evertonfs <evertonsegur5@gmail.com>
2020-06-26 00:19:43 -0300

    feat(not found handler) configured response

---
+++ core/src/main/java/com/zup/domain/exception/message/NotFoundedException.java
@@ -0,0 +1,13 @@
+package com.zup.domain.exception.message;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NotFoundedException extends Exception {
+
+  public NotFoundedException() {
+    super("NOT_FOUNDED");
+  }
+}
