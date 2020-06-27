3275cbfb6aa6c314a47bb2c6efd78ff5306fca26
evertonfs <evertonsegur5@gmail.com>
2020-06-27 14:15:33 -0300

    feat(payment) controller configured

---
+++ core/src/main/java/com/zup/infrasctructure/controller/PaymentController.java
@@ -0,0 +1,58 @@
+package com.zup.infrasctructure.controller;
+
+import com.zup.domain.dto.PaymentDTO;
+import com.zup.domain.exception.message.AlreadySavedException;
+import com.zup.domain.exception.message.NotFoundedException;
+import com.zup.domain.service.PaymentService;
+import java.util.Collection;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/payment", produces = MediaType.APPLICATION_JSON_VALUE)
+public class PaymentController implements CrudController<PaymentDTO> {
+
+  private PaymentService service;
+
+  @Override
+  @GetMapping
+  public Collection<PaymentDTO> listAll() {
+    return service.listAll();
+  }
+
+  @Override
+  @GetMapping(value = "/{id}")
+  public PaymentDTO findOneById(@NonNull @PathVariable(value = "id") UUID id)
+      throws NotFoundedException {
+    return service.findOneById(id);
+  }
+
+  @Override
+  @PostMapping
+  public PaymentDTO save(@NonNull @RequestBody PaymentDTO dto) throws AlreadySavedException {
+    return service.save(dto);
+  }
+
+  @Override
+  @PutMapping
+  public PaymentDTO update(@NonNull @RequestBody PaymentDTO dto) throws NotFoundedException {
+    return service.update(dto);
+  }
+
+  @Override
+  @DeleteMapping()
+  public PaymentDTO delete(@NonNull @RequestBody PaymentDTO dto) throws NotFoundedException {
+    return service.delete(dto);
+  }
+}
---
+++ core/src/test/java/com/zup/infrasctructure/controller/PaymentControllerTest.java
@@ -0,0 +1,99 @@
+package com.zup.infrasctructure.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.google.gson.Gson;
+import com.zup.domain.dto.PaymentDTO;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(value = PaymentController.class)
+class PaymentControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+  @MockBean private PaymentController controller;
+  private final String path = "/payment/";
+
+  @Test
+  void mustReturnStatusOkWhenPerformGetMapping() throws Exception {
+    mockMvc.perform(get(path)).andExpect(status().isOk()).andReturn();
+  }
+
+  @Test
+  void mustReturnStatusOkWhenPerformGetMappingWithId() throws Exception {
+    var uuid = UUID.randomUUID();
+    var finalPath = String.format("%s%s", path, uuid);
+
+    mockMvc
+        .perform(get(finalPath).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andReturn();
+  }
+
+  @Test
+  void mustReturnStatusOkWhenPerformPostMappingWithContentBody() throws Exception {
+    var content = new Gson().toJson(PaymentDTO.builder().build());
+
+    mockMvc
+        .perform(post(path).contentType(MediaType.APPLICATION_JSON).content(content))
+        .andExpect(status().isOk())
+        .andReturn();
+  }
+
+  @Test
+  void mustReturnStatusOkWhenPerformPutMappingWithContentBody() throws Exception {
+    var content = new Gson().toJson(PaymentDTO.builder().build());
+
+    mockMvc
+        .perform(put(path).contentType(MediaType.APPLICATION_JSON).content(content))
+        .andExpect(status().isOk())
+        .andReturn();
+  }
+
+  @Test
+  void mustReturnStatusOkWhenPerformDeleteMappingWithContentBody() throws Exception {
+    var content = new Gson().toJson(PaymentDTO.builder().build());
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.delete(path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+        .andExpect(status().isOk())
+        .andReturn();
+  }
+
+  @Test
+  void mustReturnStatusBadRequestWhenPerformPostMappingWithoutContent() throws Exception {
+    mockMvc
+        .perform(post(path).contentType(MediaType.APPLICATION_JSON).content(""))
+        .andExpect(status().is(400))
+        .andReturn();
+  }
+
+  @Test
+  void mustReturnStatusBadRequestWhenPerformPutMappingWithoutContent() throws Exception {
+    mockMvc
+        .perform(put(path).contentType(MediaType.APPLICATION_JSON).content(""))
+        .andExpect(status().is(400))
+        .andReturn();
+  }
+
+  @Test
+  void mustReturnStatusBadRequestWhenPerformDeleteMappingWithoutContent() throws Exception {
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.delete(path).contentType(MediaType.APPLICATION_JSON).content(""))
+        .andExpect(status().is(400))
+        .andReturn();
+  }
+}
