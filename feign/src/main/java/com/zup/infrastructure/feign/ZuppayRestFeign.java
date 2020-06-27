db852b2315f1e4274b957ac75aaa947520d194d5
evertonfs <evertonsegur5@gmail.com>
2020-06-27 16:39:24 -0300

    feat(feign processor) implemented

---
+++ feign/src/main/java/com/zup/infrastructure/feign/ZuppayRestFeign.java
@@ -0,0 +1,21 @@
+package com.zup.infrastructure.feign;
+
+import com.zup.domain.dto.PaymentDTO;
+import com.zup.domain.dto.TransactionDTO;
+import org.springframework.cloud.client.loadbalancer.reactive.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "zuppay-processor")
+@RequestMapping(path = "/user")
+public interface ZuppayRestFeign {
+
+  // TODO Configure Auth
+  //  @GetMapping("/current")
+  //  ResponseEntity<UserDTO> current(@RequestHeader("Authorization") String authorization);
+
+  @PostMapping("/processor/")
+  Response<TransactionDTO> createTransaction(@RequestBody PaymentDTO payment);
+}
